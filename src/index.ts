42687bb146ce10017c2194482295182a34538916
Diego Ramirez <gustavormzrmz@gmail.com>
2022-03-28 23:23:17 -0600

    added fibonacci route and default route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv';
+
+import fibonacciRoute from './route/fibonacci';
+import defaultRoute from './route/default';
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
+
+const apiBasePath = process.env.API_BASE_PATH;
+const routeFibonacciResource = process.env.ROUTE_FIBONACCI_RESOURCE;
+const routeDefaultResource = process.env.ROUTE_DEFAULT_RESOURCE;
 
 const app = express();
-const port = 8080; // default port to listen
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.use(`${apiBasePath}/${routeFibonacciResource}`, fibonacciRoute);
+
+app.get(`${routeDefaultResource}`, defaultRoute);
 
 // start the Express server
 app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+  // tslint:disable-next-line:no-console
+  console.log( `server started at http://localhost:${ port }` );
+});
